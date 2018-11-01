6586a8918218d8653da0da39794308466bde8f96
tomcheng-kaplan <tom.cheng@kaplan.com>
2018-11-01 16:57:33 -0400

    Separate repo call from dependencies call

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var graphqlReq = require('graphql-request');
+const jsonfile = require('jsonfile');
 
 var ghToken = process.env.GITHUB_API_KEY;
 
@@ -10,11 +11,12 @@ const client = new graphqlReq.GraphQLClient('https://api.github.com/graphql', {
 })
 
 var repoFilterExpr = /atom/i;
+const repoJson = 'data/repos.json'
 
 const repoQuery = `
 query getRepos ($cursor: String){
     organization(login: "KaplanTestPrep") {
-      repositories(orderBy: {field: NAME, direction: ASC}, first: 10, after: $cursor) {
+      repositories(orderBy: {field: NAME, direction: ASC}, first: 100, after: $cursor) {
         pageInfo {
           endCursor
           startCursor
@@ -42,18 +44,22 @@ function getRepos(cursor = "") {
         for (var i in repos.nodes) {
             var repo = repos.nodes[i];
             if (repoFilterExpr.test(repo.name)) {
-                getDependencies("KaplanTestPrep", repo.name, "");
+                const obj = repo.name;
+
+                jsonfile.writeFileSync(repoJson, obj, {
+                    flag: 'a'
+                })
             }
         }
         cursor = repos.pageInfo.endCursor;
         var hasNextPage = repos.pageInfo.hasNextPage;
-        // if(hasNextPage) getRepos(cursor);
+        if (hasNextPage) getRepos(cursor);
     })
 }
 
-  const dependencyQuery = `query getDependencies ($repoowner: String!, $reponame: String!, $cursor: String!){
+const dependencyQuery = `query getDependencies ($repoowner: String!, $reponame: String!, $cursor: String!){
     repository(owner: $repoowner, name: $reponame) {
-      dependencyGraphManifests(first:100,after:$cursor) {
+        dependencyGraphManifests(first:100) {
         totalCount,
         pageInfo{endCursor,hasNextPage,hasPreviousPage}
         nodes {
@@ -62,22 +68,22 @@ function getRepos(cursor = "") {
             dependencies(first: 100, after: $cursor) {
             totalCount
             pageInfo {
-              endCursor
-              hasNextPage
-              hasPreviousPage
+                endCursor
+                hasNextPage
+                hasPreviousPage
             }
             nodes {
-              hasDependencies
-              packageManager
-              packageName
-              requirements
+                hasDependencies
+                packageManager
+                packageName
+                requirements
             }
-          }
+            }
         }
-      }
+        }
     }
-  }
-  `
+}
+`
 
 function getDependencies(repoOwner, repoName, cursor = "") {
     const variables = {
@@ -90,16 +96,16 @@ function getDependencies(repoOwner, repoName, cursor = "") {
 
         for (var i in manifests) {
             var manifest = manifests[i];
-            
+
             var dependencies = manifest.dependencies;
             for (var j in dependencies.nodes) {
                 var dependency = dependencies.nodes[j];
-                console.log([repoName,manifest.filename,dependency.packageManager, dependency.packageName, dependency.requirements].join(","))
+                console.log([repoName, manifest.filename, dependency.packageManager, dependency.packageName, dependency.requirements].join(","))
             }
             cursor = dependencies.pageInfo.endCursor;
             var hasNextPage = dependencies.pageInfo.hasNextPage;
-            // if(hasNextPage) getRepos(cursor);
-            }
+            if (hasNextPage) getDependencies(repoOwner, repoName, cursor);
+        }
     }).catch(err => {
         console.log(err.response.errors) // GraphQL response errors
         console.log(err.response.data) // Response data if available    
